75c76fcdbdbeb86836427b0c8b6fc08e2657839b
Vlada <dreminava@gmail.com>
2020-08-13 22:52:28 +0300

    second task

--- script.js
+++ script.js
@@ -8,12 +8,27 @@ let appData = {
     income: [],
     savings: false
 };
-let a1 = prompt("Введите обязательную статью расходов в этом месяце",""),
-    a2 = prompt("Во сколько обойдется?",""),
-    a3 = prompt("Введите обязательную статью расходов в этом месяце",""),
-    a4 = prompt("Во сколько обойдется?","");
 
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце",""),
+        b = prompt("Во сколько обойдется?","");
+
+    if (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null
+        && a != '' && b != '' && a.length < 50) {
+        console.log("done");
+        appData.expenses[a] = b;
+    } else {
+        i--;
+    }
+
+}
 alert(appData.budget / 30);
+
+if (appData.budget <= 100) {
+     console.log("Минимальный уровень достатка");
+} else if (appData.budget > 100 && appData.budget <= 2000) {
+    console.log("Средний уровень достатка");
+} else if (appData.budget > 2000) {
+    console.log("Высокий уровень достатка");
+}
 
